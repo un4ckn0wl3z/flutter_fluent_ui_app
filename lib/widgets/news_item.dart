7d296cbec60a0fcf42bf04667cbf79c40afbe6ba
Anuwat <admin@unknownclub.net>
2022-08-30 16:12:06 +0700

    feat: edit news_item widget

--- lib/widgets/news_item.dart
+++ lib/widgets/news_item.dart
@@ -7,6 +7,7 @@ class NewsItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Typography typography = FluentTheme.of(context).typography;
     return Card(
       padding: const EdgeInsets.only(bottom: 8),
       child: Column(
@@ -33,6 +34,21 @@ class NewsItem extends StatelessWidget {
                   fit: BoxFit.cover,
                   image:
                       'https://www.infoquest.co.th/wp-content/uploads/2022/07/20220705_canva_%E0%B8%88%E0%B8%B5%E0%B8%99%E0%B8%99%E0%B8%B2%E0%B8%8B%E0%B9%88%E0%B8%B2-1024x576.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 8,
+                left: 8,
+                right: 8,
+              ),
+              child: Text(
+                'This is test message',
+                style: typography.bodyLarge?.apply(fontSizeFactor: 0.8),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ),
         ],
